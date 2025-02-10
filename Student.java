19e29dc56a493163b80567765caf9d1bb2037759
AdamGolik <156575468+AdamGolik@users.noreply.github.com>
2025-02-10 22:02:10 +0100

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,37 +1,37 @@
 package com.example.mealapp;
 
 public class Student {
-    private String firstName;
-    private String lastName;
-    private String className;
-    private String classNumber;
-    private String cardId;
+  private String firstName;
+  private String lastName;
+  private String className;
+  private String classNumber;
+  private String cardId;
 
-    public Student(String firstName, String lastName, String className, String classNumber, String cardId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.className = className;
-        this.classNumber = classNumber;
-        this.cardId = cardId;
-    }
+  public Student(String firstName, String lastName, String className, String classNumber, String cardId) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.className = className;
+    this.classNumber = classNumber;
+    this.cardId = cardId;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public String getClassName() {
-        return className;
-    }
+  public String getClassName() {
+    return className;
+  }
 
-    public String getClassNumber() {
-        return classNumber;
-    }
+  public String getClassNumber() {
+    return classNumber;
+  }
 
-    public String getCardId() {
-        return cardId;
-    }
+  public String getCardId() {
+    return cardId;
+  }
 }
